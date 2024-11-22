0cd27dbd68f8549bd6e94d7aed3b9ef8310760ea
FleurD974 <fleur.dambreville@gmail.com>
2024-11-22 18:11:13 +0100

    Live4-5-6

--- assets/modules/Home.js
+++ assets/modules/Home.js
@@ -41,7 +41,21 @@ class Home {
       .catch((error) => {
         console.log("Erreur lors de l'appel API", error)
       })
-    this.updateProjectsInfos(response.data)
+      const recentsProjects = response.data.slice(-3)
+      // Url pour récup les langages d'un projet:
+      // https://api.github.com/repos/FleurD974/{nom-du-repo}/langages
+      for (let i = 0; i < recentsProjects.length; i++) {
+        const languagesUrl = recentsProjects[i].languages_url
+        const cleanedUrl = languagesUrl.replace("https://api.github.com", "")
+        const responseLanguages = await octokit
+          .request(`GET ${cleanedUrl}`)
+          .catch((error) => {
+            console.log("Erreur lors de l'appel API", error)
+          })
+        const projectLanguages = responseLanguages.data
+        recentsProjects[i].languages = projectLanguages
+      }
+    this.updateProjectsInfos(recentsProjects)
   }
 
   updateInformation(APIdata) {
@@ -60,9 +74,20 @@ class Home {
       const project = projects[i]
       this.projectsTitle[htmlIndex].textContent = project.name
       this.projectsDescription[htmlIndex].textContent = project.description
-      const languages = project.topics
-      console.log(languages)
+      this.createHTMLLanguageTag(this.projectsTags[i], project.languages)
       htmlIndex++
+    }
+  }
+
+  createHTMLLanguageTag(div, languages) {
+    // Création dynamique 
+    // Besoin de où je crée les elements html?
+    // quels elements ? : les languages
+    const arrayLanguages = Object.keys(languages)
+    for (let i = 0; i < arrayLanguages.length; i++) {
+      const span = document.createElement('span')
+      span.textContent = arrayLanguages[i]
+      div.appendChild(span)
     }
   }
 
